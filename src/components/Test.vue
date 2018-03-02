eb3d08db49d5c9e0bfb5dedef644d32473854c47
Max Janisse <maxjanisse@gmail.com>
2018-03-01 20:25:09 -0800

    First!

---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="t">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data() {
+    return {
+      message: `Seems simple enough... ${this.$route.params.m}`,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.t {
+  font-size: 25px;
+  color: red;
+}
+</style>
